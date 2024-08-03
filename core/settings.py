7c8d37b7a6e6329b270def9977b6a8d54681489d
Fernando Mota <106290886+Motadeveloper@users.noreply.github.com>
2024-08-03 19:44:22 -0300

    1.0.4

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-89o&!**voov=3ywp!z+w6hln#!^v182p8l%kj5im6i406*kn%j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['compartil.com.br', 'www.compartil.com.br']
 
 
 # Application definition
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Define o módulo de configurações do Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# Obtém a aplicação WSGI
 application = get_wsgi_application()
